118f533cbdf50cf764308ee511156930c30cd62f
JK932-coder <ekaterina.kosmaciova@gmail.com>
2025-03-05 10:03:19 +0000

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,28 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/jekaterinavaleviciene/"
+          target="_blank"
+        >
+          Jekaterina Valeviciene
+        </a>{" "}
+        and is
+        <a
+          href="https://github.com/JK932-coder/react-weather-app"
+          target="_blank"
+        >
+          {" "}
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
        } 
 
  
