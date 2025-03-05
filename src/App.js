8ed7067f0c7de9d77818fbe693ada2749ef22d9f
JK932-coder <ekaterina.kosmaciova@gmail.com>
2025-03-05 11:51:06 +0000

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,26 +4,28 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/jekaterinavaleviciene/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Jekaterina Valeviciene
-        </a>{" "}
-        and is
-        <a
-          href="https://github.com/JK932-coder/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/jekaterinavaleviciene/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Jekaterina Valeviciene
+          </a>{" "}
+          and is
+          <a
+            href="https://github.com/JK932-coder/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
        } 
